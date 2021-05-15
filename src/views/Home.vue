4931b12b4dcd9a52362d8cbba854764835bbc0a9
rizkanfirmansyah <riezkanaprianda@gmail.com>
2021-05-16 00:11:24 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    
+    <HeaderNaima />
+    <HeroNaima />
+    <ProductsNaima />
+    <GalleryNaima />
+    <BrandsNaima />
+    <FooterNaima />
+
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import HeaderNaima from '@/components/HeaderNaima.vue';
+import HeroNaima from '@/components/HeroNaima.vue';
+import ProductsNaima from '@/components/ProductsNaima.vue';
+import GalleryNaima from '@/components/GalleryNaima.vue';
+import BrandsNaima from '@/components/BrandsNaima.vue';
+import FooterNaima from '@/components/FooterNaima.vue';
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderNaima,
+    HeroNaima,
+    ProductsNaima,
+    GalleryNaima,
+    BrandsNaima,
+    FooterNaima,
+  }
+}
+</script>
